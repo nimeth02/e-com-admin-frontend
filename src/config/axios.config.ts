14ab41d5b668367218d5870106d6827b5c1ad96d
codefusion5 <codefusionuom@gmail.com>
2024-02-13 14:16:32 +0530

    first

--- src/config/axios.config.ts
+++ src/config/axios.config.ts
@@ -1,13 +1,25 @@
 import axios from "axios"
 
-// export const axiosInstance = axios.create({
-//     baseURL: 'https://e-com-backend-4uyj.onrender.com'
+const data = localStorage.getItem("userInfo") || "";
+let authorizationHeader = {};
+
+if (data) {
+  try {
+    authorizationHeader = {
+      Authorization: `Bearer ${JSON.parse(data)}`,
+    };
+  } catch (error) {
+    console.error("Error parsing userInfo:", error);
+  }
+}
+
+export const axiosInstance = axios.create({
+  baseURL: 'https://e-com-backend-4uyj.onrender.com',
+  headers: authorizationHeader,
+});
+
+// console.log(data)
+//   export const axiosInstance = axios.create({
+//     baseURL: 'http://localhost:4020',
+//     headers:authorizationHeader 
 //   });
-const data=localStorage.getItem("userInfo") || ""
-console.log(data)
-  export const axiosInstance = axios.create({
-    baseURL: 'http://localhost:4020',
-    headers: {
-        Authorization: `Bearer ${JSON.parse(data)}`,
-      },
-  });
